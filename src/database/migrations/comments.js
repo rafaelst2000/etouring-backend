d0884aa7bd16c73160b9fc2d103e8bedf83806bf
rafaelst2000 <rafaelst2000@gmail.com>
2022-08-16 19:02:11 -0300

    ✨ add database tables

---
+++ src/database/migrations/comments.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('comments', function(table) {
+    table.increments()
+    table.dateTime('comment_at').notNullable()
+
+    table.integer('user_id').notNullable()
+    table.foreign('user_id').references('id').inTable('users')
+
+    table.integer('post_id').notNullable()
+    table.foreign('post_id').references('id').inTable('posts')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('comments')
+};
---
+++ src/database/migrations/users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments()
+    table.string('name').notNullable()
+    table.string('email').notNullable()
+    table.string('password').notNullable()
+    table.string('image').notNullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
+};
