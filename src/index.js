8ef46d57117d7dd09b48db62609957fafc49d61c
rafaelst2000 <rafaelst2000@gmail.com>
2022-09-05 19:21:24 -0300

    ✨ add 404 route

--- src/index.js
+++ src/index.js
@@ -8,4 +8,9 @@ dotenv.config()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use((req, res) => {
+  res.status(404).send({ message: 'Nothing to be seen here.' });
+})
 app.listen(3333)
+
+/* NOT FOUND */
